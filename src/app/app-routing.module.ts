14f0e9ccb0ffe654bc200d8bd44ea5fbacf2c029
paulo-789 <jorhnepaulone@gmail.com>
2020-04-13 17:07:05 +0300

    imported coponent not found in routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {SearchComponent} from './search/search.component';
 import {UsersearchComponent} from './usersearch/usersearch.component';
-
+import { NotFoundComponent } from './not-found/not-found.component';
 const routes: Routes = [
   {path:'home', component:SearchComponent},
   {path: 'search', component:UsersearchComponent},
