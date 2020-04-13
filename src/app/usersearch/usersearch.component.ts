50100931f725fbe3cc454b9b77b4f79c2751f71d
paulo-789 <jorhnepaulone@gmail.com>
2020-04-13 15:34:32 +0300

    added functionality for the search route

--- src/app/usersearch/usersearch.component.ts
+++ src/app/usersearch/usersearch.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import {GithubHttpService} from '../github-http.service';
 @Component({
   selector: 'app-usersearch',
   templateUrl: './usersearch.component.html',
   styleUrls: ['./usersearch.component.css']
 })
 export class UsersearchComponent implements OnInit {
-
-  constructor() { }
-
+  user:any;
+  repos:any
+  username:any;
+  
+  constructor(private githubhttpService:GithubHttpService) { }
+  dosearch(){
+    this.githubhttpService.updateUsername(this.username);
+ 
+    this.githubhttpService.getUser().subscribe(user=>{
+      this.user = user;
+    });
+    this.githubhttpService.getRepos().subscribe(repos=>{
+      this.repos = repos;
+    })
+  }
   ngOnInit(): void {
   }
 
