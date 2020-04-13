2f96a1ed20855dc0e9bee064edccd2c1b56d467c
paulo-789 <jorhnepaulone@gmail.com>
2020-04-13 17:08:43 +0300

    added emptypath

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
 const routes: Routes = [
   {path:'home', component:SearchComponent},
   {path: 'search', component:UsersearchComponent},
+  { path:'**', component:NotFoundComponent},
 ];
 
 @NgModule({
